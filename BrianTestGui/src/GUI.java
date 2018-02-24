01067b6cf95ef677e1612b39101c2da03de4e3cc
Brian Emmett <brian.emmett.jnr@gmail.com>
2018-02-24 18:49:01 +0000

    Got a background now okay

--- BrianTestGui/src/GUI.java
+++ BrianTestGui/src/GUI.java
@@ -1,11 +1,24 @@
+import jdk.internal.util.xml.impl.Input;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 import static java.awt.Color.BLACK;
+import static java.awt.Color.white;
 
 public class GUI {
 
     public int BOARD_SIZE,FRAME_SIZE;
+    private JLabel[] jlabels;
     public GUI(int BoardSize,int FrameSquareSize)
     {
         BOARD_SIZE=BoardSize;
@@ -13,13 +26,20 @@ public class GUI {
         JFrame frame = new JFrame("PANOPOLY");
         frame.setSize(FRAME_SIZE, FRAME_SIZE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jlabels=new JLabel[BOARD_SIZE];
         JPanel panel = new JPanel();
         panel.setLayout(null);
+        panel.setOpaque(true);
+        panel.setBackground(white);
         frame.add(panel);
         placeComponents(panel);
         // Setting the frame visibility to true
         frame.setVisible(true);
     }
+    public JLabel getPropertyLabel(int index) throws ArrayIndexOutOfBoundsException{
+        return jlabels[index];
+    }
+
     private void placeComponents(JPanel panel)
     {
         int SquaresOnSide=(((BOARD_SIZE-4)/4)+2);
@@ -29,40 +49,61 @@ public class GUI {
         int height=Offset-1,width=Offset-1;
         int NumOnBoard=0;
         Border border =BorderFactory.createLineBorder(BLACK, 1);
-        JLabel[] userLabels = new JLabel[BOARD_SIZE];
         while (x<Offset*(SquaresOnSide-1))
         {
-            updateLabels(panel,userLabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
+            updateLabels(panel,jlabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
             x+=Offset;
             NumOnBoard++;
 
         }
         while(y<(Offset*(SquaresOnSide-1)))
         {
-            updateLabels(panel,userLabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
+            updateLabels(panel,jlabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
             y+=Offset;
             NumOnBoard++;
 
         }
         while (x>=Offset)
         {
-            updateLabels(panel,userLabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
+            updateLabels(panel,jlabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
             x-=Offset;
             NumOnBoard++;
         }
         while (y>=Offset)
         {
 
-            updateLabels(panel,userLabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
+            updateLabels(panel,jlabels[NumOnBoard],x,y,height,width,NumOnBoard,border);
             y-=Offset;
             NumOnBoard++;
         }
+        JLabel image=new JLabel(new ImageIcon("BrianTestGui\\src\\ReasonsWhyBrianIsntAGraphicDesigner.png"));
+        image.setBounds((FRAME_SIZE-200)/2,(FRAME_SIZE-200)/2,200,200);//this isnt relative yet okay jeez
+        panel.add(image);
+
     }
     private void updateLabels(JPanel panel,JLabel label,int x,int y,int height, int width,int NumOnBoard,Border border)
     {
         label=new JLabel("       "+NumOnBoard);
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+            }
+        });
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+            }
+        });
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
         label.setBounds(x,y,height,width);
-        panel.add(label);
         label.setBorder(border);
+        panel.add(label);
     }
 }
---
+++ BrianTestGui/src/PropertyLabel.java
@@ -0,0 +1,7 @@
+public class PropertyLabel
+{
+    public void PropertyLabel()
+    {
+
+    }
+}
