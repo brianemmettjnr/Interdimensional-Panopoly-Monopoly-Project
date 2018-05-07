a4c8927ba4988fcff7570ff5ff59733985c7f588
brianemmettjnr <brian.emmett.jnr@gmail.com>
2018-05-07 10:47:17 +0100

    Start game has more option nows

--- InterdimensionalPanopoly/src/monopoly/Main.java
+++ InterdimensionalPanopoly/src/monopoly/Main.java
@@ -19,7 +19,7 @@ public class Main {
 	public static void createPanopoly()
 	{	
 		//allows for number of squares between 32 and 56 that are divisible by 4
-		int numLocations = 4*ThreadLocalRandom.current().nextInt(8, 14 + 1);
+		int numLocations = 4*ThreadLocalRandom.current().nextInt(8, 12 + 1);
 		new Panopoly(numLocations);
 	}
 
--- InterdimensionalPanopoly/src/monopoly/SetupGUI.java
+++ InterdimensionalPanopoly/src/monopoly/SetupGUI.java
@@ -2,12 +2,10 @@ package monopoly;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -24,43 +22,42 @@ public class SetupGUI
     static void PlayerCountGui(Panopoly panopoly1)
     {
         panopoly = panopoly1;
-        /*
+
 
         //how many players
         JFrame playerFrame= new JFrame("Interdimensional Panopoly");
         JPanel playerPanel=new JPanel();
-
-        playerFrame.setSize(400,170);
+        playerFrame.setSize(400,400);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         playerFrame.setLocation(dim.width/2-playerFrame.getSize().width/2, dim.height/2-playerFrame.getSize().height/2);
         playerFrame.add(playerPanel);
         //panel changes after here
         playerPanel.setBackground(Color.WHITE);
         playerPanel.setLayout(null);
-        JLabel image=new JLabel(new ImageIcon(GUI.class.getResource("MiniLogo.png")));
+        JLabel image=new JLabel(new ImageIcon(GUI.class.getResource("Media/Logo.png")));
         JLabel[] button=new JLabel[5];
-        Border border=BorderFactory.createLineBorder(Color.MAGENTA,2,true);
         for(int i=0;i<5;i++)
         {
-            button[i]=new JLabel();
-            button[i].setBounds(((i)*75)+7,80,70,40);
-            button[i].setBorder(border);
-            button[i].setText("  "+(i+2)+" Players");
-            button[i].setFont(new Font("Times New Roman",Font.BOLD,12));
+            button[i]=new JLabel("",SwingConstants.CENTER);
+            button[i].setBounds(((i)*75)+7,5,70,40);
+            button[i].setBorder(BorderFactory.createLineBorder(Color.white.darker().darker(),3,true));
+            button[i].setText((i+2)+" Players");
+            button[i].setFont(new Font("Chiller", Font.PLAIN,18));
+            button[i].setForeground(Color.white);
             int finalI = i;
             button[i].addMouseListener(new MouseAdapter()
             {
                 @Override
                 public void mouseEntered(MouseEvent e) {
-                    button[finalI].setText("  "+(finalI +2)+" Players?");
-                    button[finalI].setBorder(BorderFactory.createLineBorder(Color.blue,3,true));
+                    button[finalI].setText((finalI +2)+" Players?");
+                    button[finalI].setBorder(BorderFactory.createLineBorder(Color.red.darker().darker(),3,true));
                 }
             });
             button[i].addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseExited(MouseEvent e) {
-                    button[finalI].setText("  "+(finalI +2)+" Players");
-                    button[finalI].setBorder(border);
+                    button[finalI].setText((finalI +2)+" Players");
+                    button[finalI].setBorder(BorderFactory.createLineBorder(Color.white.darker().darker(),3,true));
                 }
             });
             button[i].addMouseListener(new MouseAdapter() {
@@ -70,7 +67,7 @@ public class SetupGUI
                   //set player count
                   playerFrame.dispatchEvent(new WindowEvent(playerFrame, WindowEvent.WINDOW_CLOSING));
                   playerCount = finalI + 2;
-                  PlayerNameGUI();
+                  AICountGui(playerCount);
                 }
             });
             playerPanel.add(button[i]);
@@ -78,11 +75,72 @@ public class SetupGUI
         //player select
         playerFrame.setVisible(true);
         playerFrame.setResizable(false);
-        image.setBounds(-10,0,400,100);//this isnt relative yet okay jeez
-        playerPanel.add(image);*/
+        image.setBounds(-10,0,400,400);//this isnt relative yet okay jeez
+        playerPanel.add(image);
         //AFTER HERE IS TEST INSTANTIATION;
-        PlayerNameGUI();
+        //PlayerNameGUI();
     }
+
+    static void AICountGui(int PlayerCount)
+    {
+        //how many players
+        JFrame playerFrame= new JFrame("Interdimensional Panopoly");
+        JPanel playerPanel=new JPanel();
+        playerFrame.setSize(400,400);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        playerFrame.setLocation(dim.width/2-playerFrame.getSize().width/2, dim.height/2-playerFrame.getSize().height/2);
+        playerFrame.add(playerPanel);
+        //panel changes after here
+        playerPanel.setBackground(Color.WHITE);
+        playerPanel.setLayout(null);
+        JLabel image=new JLabel(new ImageIcon(GUI.class.getResource("Media/Logo.png")));
+        JLabel[] button=new JLabel[5];
+        int width=375/PlayerCount;
+        for(int i=0;i<PlayerCount;i++)
+        {
+            button[i]=new JLabel("",SwingConstants.CENTER);
+            button[i].setBounds(((i)*width)+5,5,width,40);
+            button[i].setBorder(BorderFactory.createLineBorder(Color.white.darker().darker(),3,true));
+            button[i].setText((i)+" A.I.");
+            button[i].setFont(new Font("Chiller", Font.PLAIN,18));
+            button[i].setForeground(Color.white);
+            int finalI = i;
+            button[i].addMouseListener(new MouseAdapter()
+            {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    button[finalI].setText((finalI)+" A.I.?");
+                    button[finalI].setBorder(BorderFactory.createLineBorder(Color.red.darker().darker(),3,true));
+                }
+            });
+            button[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    button[finalI].setText((finalI)+" A.I.");
+                    button[finalI].setBorder(BorderFactory.createLineBorder(Color.white.darker().darker(),3,true));
+                }
+            });
+            button[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e)
+                {
+                    //set player count
+                    playerFrame.dispatchEvent(new WindowEvent(playerFrame, WindowEvent.WINDOW_CLOSING));
+                    int AIcount = finalI;
+                    PlayerNameGUI(AIcount);
+                }
+            });
+            playerPanel.add(button[i]);
+        }
+        //player select
+        playerFrame.setVisible(true);
+        playerFrame.setResizable(false);
+        image.setBounds(-10,0,400,400);//this isnt relative yet okay jeez
+        playerPanel.add(image);
+        //AFTER HERE IS TEST INSTANTIATION;
+        //PlayerNameGUI();
+    }
+
     private static void createIcons()
     {
         for(int i=0;i<6;i++)
@@ -102,9 +160,9 @@ public class SetupGUI
 
         panopoly.createGUI(playerArray);
     }
-    private static void PlayerNameGUI() {
+    private static void PlayerNameGUI(int AIcount) {
         createIcons();
-        /*
+        playerCount=playerCount-AIcount;
         JFrame playerFrame= new JFrame("Interdimensional Panopoly");
         JPanel playerPanel=new JPanel();
         playerFrame.setBounds(300,300,636,270);
@@ -114,8 +172,10 @@ public class SetupGUI
         playerFrame.add(playerPanel);
         playerPanel.setLayout(null);
         playerPanel.setOpaque(true);
-        playerPanel.setBackground(Color.DARK_GRAY);
+        playerPanel.setBackground(Color.red.darker().darker());
         playerFrame.setVisible(true);
+        String[] AInames={"Bob","Rob","Dob","Sob","Jim"};
+        int[] availableIcons={0,1,2,3,4,5};
         for(int i=0;i<6;i++)
         {
             JLabel image=new JLabel(new ImageIcon(GUI.images[i]));
@@ -157,14 +217,15 @@ public class SetupGUI
         }
         JLabel upperline=new JLabel("Select your Icon, then enter your username.");
         upperline.setBounds(120,10,600,20);
-        upperline.setFont(new Font("Times New Roman",Font.ITALIC,20));
+        upperline.setFont(new Font("Chiller",Font.ITALIC,28));
         upperline.setForeground(Color.white);
         playerPanel.add(upperline);
         nameSpace.setBounds(218,150,200,20);
         JButton sendinputButton=new JButton();
         sendinputButton.setBounds(218,180,200,40);
         sendinputButton.setText("PRESS TO CONFIRM");
-        sendinputButton.setFont(new Font("Times New Roman",Font.ITALIC,16));
+        sendinputButton.setFocusPainted(false);
+        sendinputButton.setFont(new Font("Chiller",Font.ITALIC,16));
         nameSpace.setText("");
         final int[] playerIncrement = {0};
         sendinputButton.addMouseListener(new MouseAdapter() {
@@ -175,6 +236,7 @@ public class SetupGUI
                     upperline.setForeground(Color.white);
                     players.add(new Player(nameSpace.getText(),selectedpictureIndex,playerIncrement[0],panopoly));
                     nameSpace.setText("");
+                    availableIcons[selectedpictureIndex]=-1;
                     selectedImage.setVisible(false);
                     selectedImage=null;
                     playerIncrement[0]++;
@@ -187,7 +249,14 @@ public class SetupGUI
                 {
                     playerFrame.dispatchEvent(new WindowEvent(playerFrame, WindowEvent.WINDOW_CLOSING));
                     //create main game GUI
-
+                    int count=0;
+                    for(int i=0;i<AIcount;i++)
+                    {
+                        while(availableIcons[count]==-1)
+                            count++;
+                        players.add(new GameBot(AInames[i],availableIcons[count],playerIncrement[0],panopoly));
+                        playerIncrement[0]++;
+                    }
                     panopoly.createGUI(players);
                 }
 
@@ -195,15 +264,15 @@ public class SetupGUI
         });
         playerPanel.add(sendinputButton);
         playerPanel.add(nameSpace);
-        *///AFTER HERE IS TEST INSTANTIATION
-        players=new ArrayList<Player>();
+        //AFTER HERE IS TEST INSTANTIATION
+        /*players=new ArrayList<Player>();
         players.add(new Player("Brian",0,0,panopoly));
         players.add(new GameBot("Chloe",1,1,panopoly));
        // players.add(new GameBot("Cian",2,2,panopoly));
 //        players.add(new GameBot("Mossy",3,3,panopoly));
 //        players.add(new GameBot("Tony",4,4,panopoly));
 //        players.add(new GameBot("Christ.........",5,5,panopoly));
-        panopoly.createGUI(players);
+        panopoly.createGUI(players);*/
     }
 
     public static void getHelp() {
