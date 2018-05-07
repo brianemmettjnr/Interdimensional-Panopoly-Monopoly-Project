9856987cad94bcda13bfd0480288dcf6cb84d371
Tomás Mc Phillips <tomas.mc-phillips@ucdconnect.ie>
2018-05-07 15:32:49 +0100

    network receiving

--- InterdimensionalPanopoly/src/monopoly/ClientHandler.java
+++ InterdimensionalPanopoly/src/monopoly/ClientHandler.java
@@ -1,35 +1,33 @@
-package monopoly;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public class ClientHandler implements Runnable{
-
-    DataInputStream in;
-    PanopolyClient client;
-    DataOutputStream out;
-
-    ClientHandler(DataInputStream in, DataOutputStream out, PanopolyClient c){
-        this.in = in;
-        this.client = c;
-        this.out = out;
-    }
-    @Override
-    public void run() {
-
-            try {
-                while(true) {
-
-                    System.out.println("hi");
-                    char status = in.readChar();
-//                System.out.println("received f");
-//                int playerId = in.readInt();
-                    client.whatUpDog(status);
-                }
-            } catch (IOException e) {
-                System.out.println("handler");
-            }
-
-        }
-    }
+//package monopoly;
+//
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+//import java.io.IOException;
+//
+//public class ClientHandler implements Runnable{
+//
+//    DataInputStream in;
+//    PanopolyClient client;
+//    DataOutputStream out;
+//
+//    ClientHandler(DataInputStream in, DataOutputStream out, PanopolyClient c){
+//        this.in = in;
+//        this.client = c;
+//        this.out = out;
+//    }
+//    @Override
+//    public void run() {
+//
+//            try {
+//
+//                    System.out.println("hi");
+//                    char status = in.readChar();
+////                System.out.println("received f");
+////                int playerId = in.readInt();
+//                    client.whatUpDog(status);
+//            } catch (IOException e) {
+//                System.out.println("handler");
+//            }
+//
+//        }
+//    }
